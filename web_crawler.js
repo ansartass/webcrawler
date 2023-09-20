591749700c7c141c156906fcd9183ed09b117a6d
ansar <ansar.tharammal@energypark.ae>
2023-09-20 17:19:25 +0400

    After Debug Session

--- web_crawler.js
+++ web_crawler.js
@@ -1,7 +1,9 @@
 import * as cheerio from 'cheerio';
-//import * as axios from 'axios';
 import axios from 'axios';
 import fs from 'fs/promises';
+//var resolveUrl = require("resolve-url")
+import resolveUrl from 'resolve-url';
+import url from 'url';
 
 // Check if the required command-line arguments are provided
 if (process.argv.length !== 4) {
@@ -32,10 +34,30 @@ async function crawl(url, currentDepth) {
             const html = response.data;
             const $ = cheerio.load(html);
 
+            // Extract background-image URL from inline CSS
+            $('*[style]').each((index, element) => {
+                const style = $(element).attr('style');
+                if (style) {
+                    const backgroundImageUrlMatches = style.match(/background-image:\s*url\(['"]?([^'"]+)['"]?\)/i);
+                    if (backgroundImageUrlMatches) {
+                        const backgroundImageUrl = backgroundImageUrlMatches[1];
+                        const absoluteBackgroundImageUrl = new URL(backgroundImageUrl, url).toString();
+                        results.push({ imageUrl: absoluteBackgroundImageUrl, sourceUrl: url, depth: currentDepth });
+                    }
+                }
+            });
+
             // Extract images and links from the HTML using Cheerio selectors
             $('img').each((index, element) => {
                 const imageUrl = $(element).attr('src');
-                results.push({ imageUrl, sourceUrl: url, depth: currentDepth });
+                if(imageUrl)
+                {
+                    //var res = getPageImg(imageUrl);                    
+                    //const absoluteImageUrl = resolveUrl(url, imageUrl);
+                    //const absoluteImageUrl = url.resolve(url, imageUrl);
+                    const absoluteImageUrl = new URL(imageUrl, url).toString(); // resolving to absolute url
+                    results.push({ imageUrl: absoluteImageUrl, sourceUrl: url, depth: currentDepth });
+                }                
             });
 
             // Extract links and crawl them if depth permits
@@ -51,7 +73,25 @@ async function crawl(url, currentDepth) {
 
                 // Recursively crawl the links
                 for (const link of links) {
-                    await crawl(link, currentDepth + 1);
+                    await crawl(link, currentDepth + 1)
+                    .then(() => {
+                        // Create a JSON object with the results
+                        const jsonObject = {
+                            results: results
+                        };
+                
+                        // Convert the JSON object to a JSON string with pretty formatting
+                        const jsonString = JSON.stringify(jsonObject, null, 2);
+                
+                        // Write the JSON data to a file
+                        return fs.writeFile('results.json', jsonString, 'utf-8');
+                    })
+                    .then(() => {
+                        console.log('Results saved to results.json');
+                    })
+                    .catch((error) => {
+                        console.error('Error writing results to file:', error);
+                    });
                 }
             }
         }
@@ -61,21 +101,42 @@ async function crawl(url, currentDepth) {
 }
 
 // Call the crawl function with the initial URL and depth
-crawl(startUrl, 0);
+crawl(startUrl, 0)
+.then(() => {
+    // Create a JSON object with the results
+    const jsonObject = {
+        results: results
+    };
 
-// Create a JSON object with the results
-const jsonObject = {
-    results: results
-};
+    // Convert the JSON object to a JSON string with pretty formatting
+    const jsonString = JSON.stringify(jsonObject, null, 2);
 
-// Convert the JSON object to a JSON string with pretty formatting
-const jsonString = JSON.stringify(jsonObject, null, 2);
+    // Write the JSON data to a file
+    return fs.writeFile('results.json', jsonString, 'utf-8');
+})
+.then(() => {
+    console.log('Results saved to results.json');
+})
+.catch((error) => {
+    console.error('Error writing results to file:', error);
+});
+    
 
-// Write the JSON data to a file
-fs.writeFile('results.json', jsonString, 'utf-8')
-    .then(() => {
-        console.log('Results saved to results.json');
-    })
-    .catch((error) => {
-        console.error('Error writing results to file:', error);
-    });
+// getPageImg(url)(()=> {
+//     return new Promise((resolve, reject) => {
+//         //get our html
+//         axios.get(url)
+//         .then(resp => {
+//             //html
+//             const html = resp.data;
+//             //load into a $
+//             const $ = cheerio.load(html);
+//             //find ourself a img
+//             const retURL = nodeURL.resolve(url,$("body").find("img")[0].attribs.src);
+//             resolve(retURL);
+//         })
+//         .catch(err => {
+//            reject(err);
+//         });
+//     });
+// });
